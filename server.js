28ab84680e7f065ba5d114ddc668cefdda1d9199
Revan kante <rohankante00@gmail.com>
2022-01-29 10:51:48 +0530

    new

---
+++ server.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var app = express();
+var path=require("path");
+var fs=require("fs");
+
+
+//server configuration
+app.use(express.static(path.join(__dirname,'public')));
+
+ app.get('/', function (req, res) {
+   res.sendFile(path.join(___dirname + '/index.html'));
+});
+ 
+/*
+app.get('/hello', function (req, res) {
+  console.log("CAlling rest api");
+});
+*/
+ 
+var server = app.listen(8081, function () {
+  console.log("Server running on 8081...")
+})
